ac6844440db53fc70095120d841f4491b67f4830
pjovanovic85 <pjovanovic85@gmail.com>
2023-07-07 23:27:07 +0200

    initial push

---
+++ src/test/java/com/projectservice/serviceapp/ServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectservice.serviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
